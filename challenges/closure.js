243d458e18696c1302ba81fc2e9f9812fe86bdec
haileyhansard <haileyhansard@gmail.com>
2020-05-22 22:44:06 -0700

    Finished Task 1 and Task 2 on the Closures File

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,17 +18,37 @@ myFunction();
 
 // Explanation: 
 
-nestedFunction can access the variable internal because of closure. 
-The child/inner function has access to variables from a higher level scope even after the variable has been called.
+nestedFunction can access the variable internal because of closure and block scope:
+The child/inner function called "nestedFunction" has access to variables from a higher level scope even after the variable has been called.
+nestedFunction has access to the variable "internal" because it is being passed down to the nestedFunction.
+The variable "internal" is declared with const which has block scope, so it is bound to the block in which it is declared, not to the function in which it is declared.
+
 
 /* Task 2: Counter */
 
-/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. 
+For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 
-let sumation = (function (){
-let counter = 0;
-console.log(function()){
-counter += 1; return counter}
-});
 
-console.log(sumation(4));
+function summation(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (let i = num - 1; i >= 1; i--) {
+    num += i;
+  }
+  return num;
+};
+
+console.log(summation(4));
+
+// AND there's also this way to do it as well!....
+
+function summation2(x) { 
+if (x === 0) {
+return 0;
+ }
+ return x + summation2(x-1);        
+}
+console.log(summation2(4));
+
+
